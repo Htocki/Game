5a1d0c2a70f121766bb10b1cd98f913e45ea27a0
Htocki <pasztownia@gmail.com>
2019-11-03 22:15:27 +0300

    Adding a Widget class.

---
+++ Space/Source/Graphics/Widget.cpp
@@ -0,0 +1,11 @@
+#include "Graphics/Widget.hpp"
+
+Widget::Widget (sf::Vector2f position) {
+    position_ = position;
+}
+
+Widget::~Widget () {}
+
+sf::Vector2f Widget::getPosition () const {
+    return position_;
+}

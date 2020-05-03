b6ad09268b3ff8b233e2df50ff355f61ec626b27
Htocki <pasztownia@gmail.com>
2020-05-04 01:54:40 +0300

    The implementation of the Subject class has been supplemented.

--- game/lib/utility/src/Subject.cpp
+++ game/lib/utility/src/Subject.cpp
@@ -1,12 +1,18 @@
 #include "Subject.h"
 
+#include <algorithm>
+
 namespace Game {
   void Subject::Attach(Observer* observer) {
     m_observers.push_back(observer);
   }
 
   void Subject::Detach(Observer* observer) {
-    // Найти указанного наблюдателя и удалить из вектора
+    auto new_end = std::remove(
+      std::begin(m_observers),
+      std::end(m_observers),
+      observer);
+    m_observers.erase(new_end, m_observers.end());
   }
 
   void Subject::Notify() {
